0028f096fec1c788bf35a94ff13ff65d446c2dbd
BrianDYKim <18k7102dy@naver.com>
2022-03-29 23:29:50 +0900

    save search.html

---
+++ src/main/java/com/example/devopstest/controller/web/WebResourceController.java
@@ -0,0 +1,15 @@
+package com.example.devopstest.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Doyeop Kim
+ * @version 0.0
+ * @since 2022/03/27
+ */
+@Controller
+@RequestMapping("/map")
+public class WebResourceController {
+
+}
